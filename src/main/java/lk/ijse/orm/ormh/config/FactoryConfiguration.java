a003dce0c0f847eedcdb1f414e14e3c5846da0c7
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-25 11:49:56 +0530

    config code ok.

--- src/main/java/lk/ijse/orm/ormh/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/orm/ormh/config/FactoryConfiguration.java
@@ -1,4 +1,36 @@
 package lk.ijse.orm.ormh.config;
 
+import lk.ijse.orm.ormh.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private final SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+        Configuration config = new Configuration();
+        config.configure();
+        // has call entity classes.
+        config.addAnnotatedClass(Admin.class)
+                .addAnnotatedClass(Programme.class)
+                .addAnnotatedClass(Reception.class)
+                .addAnnotatedClass(Patients.class)
+                .addAnnotatedClass(Sessions.class)
+                .addAnnotatedClass(Therapists.class)
+                .addAnnotatedClass(Payment.class);
+        sessionFactory = config.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getFactoryConfiguration() {
+        return factoryConfiguration != null ? factoryConfiguration : new FactoryConfiguration();
+        // return factoryConfiguration == null ? new FactoryConfiguration() : factoryConfiguration;
+    }
+
+    public Session getSessionFactory() {
+        return sessionFactory.openSession();
+    }
 }
+
+
