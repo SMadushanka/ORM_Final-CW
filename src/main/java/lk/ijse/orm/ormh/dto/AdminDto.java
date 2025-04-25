b754055efa26731a87b601111674d89c3318e6f3
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-25 13:25:55 +0530

    all done.

--- src/main/java/lk/ijse/orm/ormh/dto/AdminDto.java
+++ src/main/java/lk/ijse/orm/ormh/dto/AdminDto.java
@@ -1,4 +1,15 @@
 package lk.ijse.orm.ormh.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class AdminDto {
+    private String name;
+    private String email;
+    private String password;
+    private int contact;
 }
