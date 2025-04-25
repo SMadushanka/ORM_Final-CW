51419dbd7cf44a3f137417c789b1b9640c4252a0
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-25 13:26:23 +0530

    code done.

--- src/main/java/lk/ijse/orm/ormh/controller/LandingController.java
+++ src/main/java/lk/ijse/orm/ormh/controller/LandingController.java
@@ -3,6 +3,9 @@ package lk.ijse.orm.ormh.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.orm.ormh.utils.WindowUtils;
+
+import java.io.IOException;
 
 public class LandingController {
 
@@ -10,13 +13,12 @@ public class LandingController {
     private AnchorPane LandingPane;
 
     @FXML
-    void gotoSigInView(ActionEvent event) {
-
+    void gotoSigInView(ActionEvent event) throws Exception {
+        new WindowUtils().navigateTo("RoleView", LandingPane);
     }
 
     @FXML
-    void gotoSignUpView(ActionEvent event) {
-
+    void gotoSignUpView(ActionEvent event) throws Exception {
+        new WindowUtils().navigateTo("RoleView", LandingPane);
     }
-
 }
