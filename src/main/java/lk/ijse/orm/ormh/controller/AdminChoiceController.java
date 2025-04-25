c783978731c8b8725404ac16175caa0d96615a35
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-25 12:12:53 +0530

    admin Choice controller code ok.

--- src/main/java/lk/ijse/orm/ormh/controller/AdminChoiceController.java
+++ src/main/java/lk/ijse/orm/ormh/controller/AdminChoiceController.java
@@ -3,6 +3,9 @@ package lk.ijse.orm.ormh.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.orm.ormh.utils.WindowUtils;
+
+import java.io.IOException;
 
 public class AdminChoiceController {
 
@@ -10,18 +13,17 @@ public class AdminChoiceController {
     private AnchorPane pane;
 
     @FXML
-    void gotoBack(ActionEvent event) {
-
+    void gotoBack(ActionEvent event) throws Exception {
+        new WindowUtils().navigateTo("Landing", pane);
     }
 
     @FXML
-    void gotoTherapists(ActionEvent event) {
-
+    void gotoTherapists(ActionEvent event) throws Exception {
+        new WindowUtils().navigateTo("TherapistView", pane);
     }
 
     @FXML
-    void gotoTherapyProgramme(ActionEvent event) {
-
+    void gotoTherapyProgramme(ActionEvent event) throws Exception {
+        new WindowUtils().navigateTo("ProgrammeView", pane);
     }
-
 }
