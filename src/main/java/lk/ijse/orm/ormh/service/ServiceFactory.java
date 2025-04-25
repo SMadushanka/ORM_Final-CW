17ba6c73f1536ab5e62e5a960e230ee0c0852502
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-25 13:31:13 +0530

    error .not done yet.

--- src/main/java/lk/ijse/orm/ormh/service/ServiceFactory.java
+++ src/main/java/lk/ijse/orm/ormh/service/ServiceFactory.java
@@ -1,4 +1,40 @@
 package lk.ijse.orm.ormh.service;
 
+import lk.ijse.orm.ormh.service.custom.impl.*;
+
+import java.beans.beancontext.BeanContextServiceProvider;
+
 public class ServiceFactory {
+    private static ServiceFactory serviceFactory;
+
+    private ServiceFactory(){}
+
+    public static ServiceFactory getServiceFactory(){
+        return serviceFactory != null ? serviceFactory : new ServiceFactory();
+    }
+
+    public SuperService getService(serviceType type){
+        switch (type) {
+            case ADMIN:
+                return new AdminServiceImpl();
+            case RECEPTION:
+                return new ReceptionServiceImpl();
+            case PROGRAMME:
+                return new ProgrammeServiceImpl();
+            case THERAPIST:
+                return new TherapistServiceImpl();
+            case SESSIONS:
+                return new SessionServiceImpl();
+            case PATIENT:
+                return new PatientServiceImpl();
+            case PAYMENT:
+                return new PaymentServiceImpl();
+            default:
+                return null;
+        }
+    }
+
+    public enum serviceType{
+        ADMIN,RECEPTION,PROGRAMME,THERAPIST,SESSIONS,PATIENT,PAYMENT
+    }
 }
