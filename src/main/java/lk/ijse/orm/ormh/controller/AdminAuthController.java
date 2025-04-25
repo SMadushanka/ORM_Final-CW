51362d517e1f2e59d74aa34cd973d5b073c64809
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-25 12:06:16 +0530

    admin controller code ok.

--- src/main/java/lk/ijse/orm/ormh/controller/AdminAuthController.java
+++ src/main/java/lk/ijse/orm/ormh/controller/AdminAuthController.java
@@ -2,12 +2,21 @@ package lk.ijse.orm.ormh.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.orm.ormh.exception.LoginException;
+import lk.ijse.orm.ormh.service.ServiceFactory;
+import lk.ijse.orm.ormh.service.custom.AdminService;
+import lk.ijse.orm.ormh.utils.WindowUtils;
+
+import java.io.IOException;
 
 public class AdminAuthController {
+
+    AdminService adminService = (AdminService) ServiceFactory.getServiceFactory().getService(ServiceFactory.serviceType.ADMIN);
 
     @FXML
     private AnchorPane adminLoginAuthPane;
@@ -19,18 +28,30 @@ public class AdminAuthController {
     private PasswordField passwordTxt;
 
     @FXML
-    void backLanding(ActionEvent event) {
-
+    void backLanding(ActionEvent event) throws IOException {
+        new WindowUtils().navigateTo("RoleView", adminLoginAuthPane);
     }
 
     @FXML
-    void gotoChoicesView(ActionEvent event) {
-
+    void gotoChoicesView(ActionEvent event) throws IOException {
+        new WindowUtils().navigateTo("AdminView", adminLoginAuthPane);
     }
 
     @FXML
-    void gotoRegister(MouseEvent event) {
+    void gotoRegister(MouseEvent event) throws Exception {
+        try {
+            String email = emailTxt.getText();
+            String password = passwordTxt.getText();
 
+            boolean resp = adminService.authAdmin(email, password);
+
+            if (resp) {
+                new WindowUtils().navigateTo("AdminChoicesView", adminLoginAuthPane);
+            }else{
+                new Alert(Alert.AlertType.ERROR, "Something Went Wrong!").show();
+            }
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
     }
-
 }
