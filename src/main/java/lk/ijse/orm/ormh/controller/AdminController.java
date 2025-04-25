b4eb11c0b40953d88f50aa7945b9633b3258fba5
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-25 13:25:27 +0530

    code written don this class.

--- src/main/java/lk/ijse/orm/ormh/controller/AdminController.java
+++ src/main/java/lk/ijse/orm/ormh/controller/AdminController.java
@@ -2,11 +2,22 @@ package lk.ijse.orm.ormh.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.orm.ormh.dto.AdminDto;
+import lk.ijse.orm.ormh.exception.RegisterException;
+import lk.ijse.orm.ormh.service.ServiceFactory;
+import lk.ijse.orm.ormh.service.custom.AdminService;
+import lk.ijse.orm.ormh.utils.WindowUtils;
+
+import java.io.IOException;
 
 public class AdminController {
+
+    AdminService adminService = (AdminService) ServiceFactory.getServiceFactory().getService(ServiceFactory.serviceType.ADMIN);
+
 
     @FXML
     private TextField contactTxt;
@@ -24,18 +35,54 @@ public class AdminController {
     private AnchorPane pane;
 
     @FXML
-    void gotoBack(ActionEvent event) {
-
+    void gotoBack(ActionEvent event) throws Exception {
+        new WindowUtils().navigateTo("RoleView", pane);
     }
 
     @FXML
-    void gotoLog(MouseEvent event) {
-
+    void gotoLog(MouseEvent event) throws Exception {
+        new WindowUtils().navigateTo("AdminAuthView", pane);
     }
 
     @FXML
-    void saveAdmin(ActionEvent event) {
+    void saveAdmin(ActionEvent event) throws Exception {
+        String emailReg = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        String conReg = "^[0-9]{10}$";
 
+        if (emailTxt.getText().isEmpty() || contactTxt.getText().isEmpty() || nameTxt.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Something Went Wrong!").show();
+        }
+
+        if (!emailTxt.getText().matches(emailReg)) {
+            new Alert(Alert.AlertType.ERROR, "Invalid Email Address!").show();
+            return;
+        }
+
+        if (!contactTxt.getText().matches(conReg)) {
+            new Alert(Alert.AlertType.ERROR, "Invalid Contact Number!").show();
+            return;
+        }
+
+        try {
+            boolean resp = adminService.saveAdmin(new AdminDto(
+                    nameTxt.getText(), emailTxt.getText(), passwordTxt.getText(), Integer.parseInt(contactTxt.getText())));
+
+            if (resp) {
+                new Alert(Alert.AlertType.INFORMATION, "New Admin Added Success!").show();
+                clearFields();
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Something Went Wrong!").show();
+            }
+        } catch (RegisterException e) {
+            e.printStackTrace();
+        }
     }
 
+    private void clearFields() {
+        nameTxt.setText("");
+        emailTxt.setText("");
+        passwordTxt.setText("");
+        contactTxt.setText("");
+    }
 }
