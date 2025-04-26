7721f4a6b1504f8f318abca5149239a265f12813
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-26 12:06:19 +0530

    bug fix. and no error.

--- src/main/java/lk/ijse/orm/ormh/entity/Programme.java
+++ src/main/java/lk/ijse/orm/ormh/entity/Programme.java
@@ -1,4 +1,28 @@
 package lk.ijse.orm.ormh.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "programme")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Programme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String duration;
+    private double fees;
+
+    public Programme(String name, String duration, double fees) {
+        this.name = name;
+        this.duration = duration;
+        this.fees = fees;
+    }
 }
