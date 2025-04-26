be66b40323d9e410d9a959c949ee05d75167b6f1
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-26 12:10:22 +0530

    error fixe

--- src/main/java/lk/ijse/orm/ormh/dto/TherapistDto.java
+++ src/main/java/lk/ijse/orm/ormh/dto/TherapistDto.java
@@ -1,4 +1,25 @@
 package lk.ijse.orm.ormh.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TherapistDto {
+    private int id;
+    private String name;
+    private String email;
+    private int phone;
+
+    public TherapistDto(String name, String email, int phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public TherapistDto(String name) {
+        this.name = name;
+    }
 }
