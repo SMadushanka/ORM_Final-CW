b8492cafb8f5cb78018caa29d42e36b0b757d51b
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-26 12:11:32 +0530

    delete class ok.

--- src/main/java/lk/ijse/orm/ormh/entity/Therapists.java
+++ src/main/java/lk/ijse/orm/ormh/entity/Therapists.java
@@ -1,4 +1,28 @@
 package lk.ijse.orm.ormh.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "therapist")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Therapists {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int id;
+   private String name;
+   private String email;
+   private int phone;
+
+    public Therapists(String name, String email, int phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
 }
