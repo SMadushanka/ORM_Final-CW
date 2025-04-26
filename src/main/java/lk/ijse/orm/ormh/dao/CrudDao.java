69e005fdd2f7655d7813aaa5b95e32e6cfae2f20
WSmadushanka <wijamu.maduu22@gmail.com>
2025-04-26 12:03:57 +0530

    no error.

--- src/main/java/lk/ijse/orm/ormh/dao/CrudDao.java
+++ src/main/java/lk/ijse/orm/ormh/dao/CrudDao.java
@@ -1,4 +1,13 @@
 package lk.ijse.orm.ormh.dao;
 
-public interface CrudDao {
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudDao <T> extends SuperDao {
+    boolean save(T entity) throws Exception;
+    boolean update(T t);
+    boolean deleteByPK(int id) throws Exception;
+    List<T> getAll();
+    T findByPK(int id);
+    Optional<String> getLastPK();
 }
--- src/main/java/lk/ijse/orm/ormh/dao/DaoFactory.java
+++ src/main/java/lk/ijse/orm/ormh/dao/DaoFactory.java
@@ -1,4 +1,39 @@
 package lk.ijse.orm.ormh.dao;
 
+import lk.ijse.orm.ormh.dao.custom.impl.*;
+
 public class DaoFactory {
+    private static DaoFactory daoFactory;
+
+    private DaoFactory(){}
+
+    public static DaoFactory getDaoFactory(){
+        return daoFactory != null ? daoFactory : new DaoFactory();
+    }
+
+    public SuperDao getDao(daoType type){
+        switch (type) {
+            case ADMIN:
+                return new AdminDaoImpl();
+            case RECEPTION:
+                return new ReceptionDaoImpl();
+            case PROGRAMME:
+                return new ProgrammeDaoImpl();
+            case THERAPIST:
+                return new TherapistDaoImpl();
+            case SESSIONS:
+                return new SessionDaoImpl();
+            case PATIENT:
+                return new PatientDaoImpl();
+            case PAYMENT:
+                return new PaymentDaoImpl();
+            default:
+                return null;
+        }
+    }
+
+    public enum daoType{
+        ADMIN,RECEPTION,PROGRAMME,THERAPIST,SESSIONS,PATIENT,PAYMENT
+    }
 }
+
